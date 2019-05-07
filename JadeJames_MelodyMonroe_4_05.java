f281404b078e52370afccf95a2f528a98d13a47c
mnmonroe <mnmonroe@gmail.com>
2019-05-06 21:29:58 -0600

    Completed program

--- JadeJames_MelodyMonroe_4_05.java
+++ JadeJames_MelodyMonroe_4_05.java
@@ -1,15 +1,20 @@
 /** 
-   Purpose: This program calculates gross pay, net pay, 
+   Purpose: This program calculates employee gross pay, net pay, 
    wealth, taxes, hours and pay rate based on data 
-   input file.
+   input file to an output file and console.
    
     @author Jade James & Melody Monroe
     Project #5, CS 1050, Section 4
     IDE: jGrasp
     
-    WORD AND DEFINITION
+    Bumbershoot: noun informal - an umbrella. 
+    Source: https://www.dictionary.com/browse/bumbershoot
     
-    QUOTE
+    “A single conversation across the table with a wise man is better 
+    than ten years mere study of books." 
+    - Henry Wadsworth Longfellow (1807 – 1882)
+    Source: https://www.brainyquote.com/citation/authors/henry_wadsworth_longfello
+    
 
 */
 
@@ -21,7 +26,8 @@ import java.util.ArrayList;   // Access array list class
 
 
 public class JadeJames_MelodyMonroe_4_05 {
-  
+   
+   // Accesses the toolkit
    static Toolkit tools = new Toolkit(); 
   
    public static void main(String[ ] args) throws IOException {
@@ -33,35 +39,27 @@ public class JadeJames_MelodyMonroe_4_05 {
       final String EMPLOYEE_INPUT  = "EmployeeInput.txt";
       final String EMPLOYEE_PARAMETERS = "EmployeeParameters.txt";
       
-      int maxEmployees;
-      int numRead = 0;
-      int sortResultEmployee = 0;
-      int sortResultGrossPay = 0;
-      double savingsRate;
-      double iraRate;
-      double fedRate;
-      double stateRate;
-      String warning;
+      int maxEmployees; // Counts employee entries
+      int numRead = 0; // Counts number of values read
+      int sortResult = 0; // Sorts results using Employee class
+      double savingsRate; // Savings rate from Employee parameters
+      double iraRate; // IRA rate from Employee parameters
+      double fedRate; // Federal tax rate from Employee parameters
+      double stateRate; // State tax rate from Employee parameters
       
-      // Input error checking
-      //try {
-        // File inputDataFile = new File(INPUT_FILE);
-        // Scanner inputFile  = new Scanner(inputDataFile);
-     // }
-      //catch(IOException e) {
-       //  System.out.print ("Input file not found.");
-     // }
+      // Access toolkit for formatting data
+      DecimalFormat dollars = new DecimalFormat ("$#,##0.00");
+      DecimalFormat leadingBlank = new DecimalFormat ("#.00");
       
+      // Access input data
       File inputDataFile = new File(INPUT_FILE);
       Scanner inputFile  = new Scanner(inputDataFile);
-      DecimalFormat dollars = new DecimalFormat ("$#,##0.00");
-      DecimalFormat leadingBlank = new DecimalFormat ("#.00");    
-      
+          
       // Output data to file and console
       FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
       PrintWriter outputFile = new PrintWriter(outputDataFile);
 
-      // Access array
+      // Access employee array
       EmployeeParameters param = new EmployeeParameters();
       
       param.getEmployeeParameters();
@@ -73,29 +71,32 @@ public class JadeJames_MelodyMonroe_4_05 {
       fedRate = param.federalWithholdingRate;
       stateRate = param.stateWithholdingRate;
       
-     
+      // Collect each value as instatiation of Employee array      
       Employee[] empl = new Employee[maxEmployees]; 
       
+      // Reads the values from the input file
       numRead = readData (inputFile, empl);
               
       // Dispay the employee parameters
       param.displayEmployeeParameters();
       outputFile.println();
       System.out.println();
-               
+      
+      // Passes values to the grossPay method         
       grossPay (empl, numRead);
-   
+      
+      // Passes employee data to calcDeductions method 
       calcDeductions (empl, iraRate, fedRate, stateRate, savingsRate, numRead);
       
       // Output data to the console and output file
       printReport(outputFile,"Input", empl, numRead); 
       
       // Sort data by employee name
-      sortResultEmployee = Employee.selectionSortArrayOfClass(empl, numRead, "Name");
+      sortResult = Employee.selectionSortArrayOfClass(empl, numRead, "Name");
       printReport (outputFile,"Name", empl, numRead);
       
       // Sort data by ascending gross pay
-      sortResultGrossPay = Employee.selectionSortArrayOfClass(empl, numRead, "Gross Pay");
+      sortResult = Employee.selectionSortArrayOfClass(empl, numRead, "Gross Pay");
       printReport (outputFile, "Gross Pay", empl, numRead);
       
       inputFile.close(); // Close input file
@@ -104,14 +105,14 @@ public class JadeJames_MelodyMonroe_4_05 {
       System.exit(0);  // Exit program
    } // End main
 
+// Reads employee data provided in input file
    //***********************************************************************************
-   // Reads employee data provided in input file
    public static int readData (Scanner input, Employee[] empl) {
          
       int numRead = 0; // Number of lines read
          
       while(input.hasNext() && numRead < empl.length) { 
-         empl[numRead] = new Employee(); // Creates new instance of Employee for each entry
+         empl[numRead] = new Employee(); // New instance of Employee for each entry
          empl[numRead].payRate = input.nextDouble();
          empl[numRead].hoursWorked = input.nextDouble();
          empl[numRead].name = input.nextLine();
@@ -121,9 +122,8 @@ public class JadeJames_MelodyMonroe_4_05 {
       return numRead;
    } // End readData 
 
-    
+// Calculates gross pay values for each employee    
    //*************************************************************************************
-   // Gross pay calculation
    public static void grossPay(Employee[] empl, int numRead) {
          
       for (int i = 0; i < numRead; i++) {
@@ -144,8 +144,9 @@ public class JadeJames_MelodyMonroe_4_05 {
          } 
       }
    } //end grossPay
+
+// Calculates employee deductions
    //*****************************************************************************************
-   // Calculate deductions
    public static void calcDeductions(Employee[] empl,double ira,double fedRate,
         double stateRate,double saveRate,int numRead) {
         
@@ -165,8 +166,8 @@ public class JadeJames_MelodyMonroe_4_05 {
       }
    }
 
+// Prints heading row for report to console and file
    //**************************************************************************************************
-   // Print heading row to report
    public static void printHeader (PrintWriter output, String order) {
          
       String reportHeading;
@@ -178,30 +179,31 @@ public class JadeJames_MelodyMonroe_4_05 {
          "\r\n" + "\r\n" +
          // table headers
          tools.padString("Name", 21) +
-         " " + tools.padString("Gross Pay", 10) +
-         " " + tools.padString("Net Pay", 8) +
-         " " + tools.padString("Wealth", 10) +
-         " " + tools.padString("Taxes", 8) +
-         " " + tools.padString("Hours", 7) +
-         " " + tools.padString("Pay Rate", 0) +
+         " " + tools.padString("Gross Pay", 12) +
+         " " + tools.padString("Net Pay", 12) +
+         " " + tools.padString("Wealth", 11) +
+         " " + tools.padString("Taxes", 10) +
+         " " + tools.padString("Hours", 8) +
+         " " + tools.padString("Pay Rate", 8) +
          " " + "\r\n" +
          tools.padString("---------------", 21) +
-         " " + tools.padString("---------", 10) +
-         " " + tools.padString("-------", 8) +
+         " " + tools.padString("---------", 12) +
+         " " + tools.padString("-------", 12) +
+         " " + tools.padString("-------", 11) +
          " " + tools.padString("-------", 10) +
          " " + tools.padString("-------", 8) +
-         " " + tools.padString("-------", 6) +
-         " " + tools.padString("--------", 0) +
-         " ";
+         " " + tools.padString("--------", 8);
          
       output.println(reportHeading);
       System.out.println(reportHeading);
    } // End printHeader
-   
+
+// Calculates totals for all employee data
    //**************************************************************************************************
-   // Calculate the totals
    public static void getTotals(Employee[] empl, PrintWriter output, int numRead) {
    
+      DecimalFormat dollars = new DecimalFormat ("$#,##0.00");
+      DecimalFormat leadingBlank = new DecimalFormat ("#.00"); 
       final String DOLLAR = "##,##0.00";
    
       String str;
@@ -230,14 +232,14 @@ public class JadeJames_MelodyMonroe_4_05 {
       }
    
    // Print out all the sums and the average
-      str = "Totals: " +
-            tools.leftPad(sumGrossPay, 22, DOLLAR) +
-            tools.leftPad(sumNetPay, 13, DOLLAR) +
-            tools.leftPad(sumWealth, 11, DOLLAR) +
-            tools.leftPad(sumTaxes, 12, DOLLAR) +
-            tools.leftPad(sumHours, 11, DOLLAR) +
+      str = "Totals: " + "\t\t\t\t\t" +
+            dollars.format(sumGrossPay) + "\t" +
+            dollars.format(sumNetPay) + "\t" +
+            dollars.format(sumWealth) + "\t " + 
+            dollars.format(sumTaxes) + " \t\t" + 
+            leadingBlank.format(sumHours) + "\t\t" +
             "\r\n" + tools.padString("Average: ", 83, " ", "") +
-            tools.leftPad(avgPayRate, 5, DOLLAR) +
+            dollars.format(avgPayRate) +
             "\r\n\r\n" +
             "The total number of employees processed: " +
             
@@ -246,10 +248,10 @@ public class JadeJames_MelodyMonroe_4_05 {
       System.out.println(str.trim());
       output.println(str.trim());
    
-   }
+   }// End getTotals
+
+//Prints full report with employee data and calculations   
    //**************************************************************************************************
-   
-// Run all the methods to output data
    public static void printReport(PrintWriter output,String order,Employee[] empl, 
                      int numRead) {
    
@@ -258,13 +260,18 @@ public class JadeJames_MelodyMonroe_4_05 {
       getTotals(empl, output, numRead);
    
     }
+   
+// Prints employee pay, deductions and savings  
    //************************************************************************************************** 
-// Print out data in a table
    public static void outputData(PrintWriter output, Employee[] empl, int numRead) {
    
-      final String DOLLAR = "##,##0.00";
+      DecimalFormat dollars = new DecimalFormat ("$#,##0.00");
+      DecimalFormat leadingBlank = new DecimalFormat ("#.00");
    
+      final String DOLLAR = "$##,##0.00";
+       
       double wealth = 0.0;
+      
    
       for(int i = 0; i < numRead; i++) {
       
@@ -272,21 +279,22 @@ public class JadeJames_MelodyMonroe_4_05 {
       
          String str;
       
-         str = tools.padString(empl[i].name, 19) + " " + tools.leftPad(empl[i].grossPay, 8, DOLLAR) +
-            " " + tools.leftPad(empl[i].netPay, 10, DOLLAR) + " " + tools.leftPad(wealth, 8, DOLLAR) +
-            " " + tools.leftPad(empl[i].taxAmount, 9, DOLLAR) + 
-            " " + tools.leftPad(empl[i].hoursWorked, 8, DOLLAR) +
-            " " + tools.leftPad(empl[i].payRate, 8, DOLLAR) +
-            " ";
+         str = tools.padString(empl[i].name, 21) + "\t\t" +
+            dollars.format(empl[i].grossPay) + "\t\t" + 
+            dollars.format(empl[i].netPay) + "\t\t" + 
+            dollars.format(wealth) + "\t\t" + 
+            dollars.format(empl[i].taxAmount) + "\t\t" + 
+            leadingBlank.format(empl[i].hoursWorked) + "\t\t" + 
+            dollars.format(empl[i].payRate);
       
          output.println(str.trim());
          System.out.println(str.trim());
       
       }
    } // End outputData
-  //*************************************************************************************************************
-  //printdetails method
 
+// Prints details to the console and to output file   
+  //*************************************************************************************************************
      public static void printDetails(Employee[] empl, PrintWriter output, int numRead) {
         
         final String DOLLAR = "$##,##0.00"; 
